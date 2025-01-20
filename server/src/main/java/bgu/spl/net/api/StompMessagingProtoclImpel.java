d1859bd1e7197076ae142f421ff55169f89f9733
HilaCohenn <cohenhila10@gmail.com>
2025-01-20 14:22:22 +0000

    protocol update

--- server/src/main/java/bgu/spl/net/api/StompMessagingProtoclImpel.java
+++ server/src/main/java/bgu/spl/net/api/StompMessagingProtoclImpel.java
@@ -9,19 +9,61 @@ public class StompMessagingProtoclImpel<T> implements StompMessagingProtocol<T> 
 
     @Override
     public void start(int connectionId, Connections<T> connections) {
-        this.connections = (ConnectionsImpl<T>) connections;
+        this.connections = (ConnectionsImpl<StompFrame>) connections;
         this.connectionId = connectionId;
     }
 
-    @Override
-    public void process(T message) {
-        if(message.equals("disconnect")){//comand = disconnect
-            shouldTerminate = true;
-        }
-        else{
-           this.connections.send(connectionId, message);
-        }
+        @Override
+    public void process(StompFrame frame) {
+        String command = frame.getCommand();
 
+        switch (command.toUpperCase()) {
+            case "CONNECT":
+                handleConnect(parts[1]);
+                break;
+            case "DISCONNECT":
+                handleDisconnect();
+                break;
+            case "SEND":
+                handleSend(parts[1]);
+                break;
+            case "SUBSCRIBE":
+                handleSubscribe(parts[1]);
+                break;
+            default:
+                System.out.println("Unknown command: " + command);
+                // send an ERROR
+                break;
+        }
+    }
+
+    private void handleConnect(String details) {
+        // Handle connect logic
+    }
+
+    private void handleDisconnect() {
+        shouldTerminate = true;
+        // Handle disconnect logic
+    }
+
+    private void handleSend(String details) {
+        String destination = frame.getHeaders().get("destination");
+        if (destination == null) {
+            //  send the client an ERROR frame and then close the connection
+            System.out.println("SEND command missing destination header");
+            return;
+        }
+        boolean isSent = connections.send(destination, frame);
+        if (isSent){
+            // send the client a RECEIPT frame
+        } else {
+            // send the client an ERROR frame
+        }
+        // Handle send logic
+    }
+
+    private void handleSubscribe(String details) {
+        // Handle subscribe logic
     }
 
     @Override
